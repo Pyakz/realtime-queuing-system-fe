db69b6ca1db9d16d9c935d1140a24353a1d80c84
Pyakz <MarkVergelBanguis@gmail.com>
2021-10-25 17:08:59 +0800

    asd

--- src/routes/PublicRoute.tsx
+++ src/routes/PublicRoute.tsx
@@ -26,7 +26,7 @@ const PublicRoute = ({ component: Component, restricted, ...rest }: any) => {
     return <FullScreenSpinner />;
   } else {
     if (loggedIn && restricted) {
-      return <Redirect to="/" />;
+      return <Redirect to="/dashboard" />;
     } else {
       return (
         <Route
