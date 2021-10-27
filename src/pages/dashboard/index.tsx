6ad510764f1cc64235dd540ce6f06135dbcfc31c
Pyakz <MarkVergelBanguis@gmail.com>
2021-10-27 20:57:34 +0800

    scanner

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,10 +1,17 @@
 import { useAuth } from "../../context/AuthContext";
 import AdminPage from "../admin";
 import StaffPage from "../staff";
+import Scanner from "../staff/counter/Scanner";
 
 const Dashboard = () => {
   const { auth } = useAuth();
-  return auth.role === "ADMIN" ? <AdminPage /> : <StaffPage />;
+  return auth.role === "ADMIN" ? (
+    <AdminPage />
+  ) : auth.role === "ADMIN" ? (
+    <StaffPage />
+  ) : (
+    <Scanner />
+  );
 };
 
 export default Dashboard;
